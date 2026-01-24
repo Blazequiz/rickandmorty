445c1ad1ed5964f05afc60c2fa41427ea07c6c2a
Hlib <glebmalonos@gmail.com>
2026-01-24 22:44:34 +0200

    final

--- src/JS/episodes-js/episodes-heder.js
+++ src/JS/episodes-js/episodes-heder.js
@@ -1,12 +1,24 @@
-const image = document.querySelector("#header_text_logo_img")
-const originalImage = './img/together/text_logo.png';
+const image = document.querySelector('#header_text_logo_img');
+// const originalImage = './img/together/text_logo.png';
 // const hoverImage = './img/together/text_logo_gl.png'; // Зображення для hover
-import hoverImage from '../../public/text_logo_gl.png'
+import hoverImage from '../../public/text_logo_gl.png';
+import originalImage from '../../public/text_logo.png';
+// 2x
+import hoverImage2x from '../../public/text_logo_gl_2x.png';
+import originalImage2x from '../../public/text_logo_2x.png';
 
 image.addEventListener('mouseover', () => {
+  if (window.devicePixelRatio > 1) {
+    image.src = hoverImage2x;
+  } else {
     image.src = hoverImage;
+  }
 });
 
 image.addEventListener('mouseout', () => {
+      if (window.devicePixelRatio > 1) {
+    image.src = originalImage2x;
+  } else {
     image.src = originalImage;
+  }
 });
